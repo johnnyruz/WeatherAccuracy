a25d2c5adebf5bfd24fa6e30130fda4ab1f83cac
Johnnyruzdev <Johnnyruzdev01@outlook.com>
2023-05-22 22:36:40 -0400

    update variables & function app settings

--- IaC/main.tf
+++ IaC/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 resource "azurerm_storage_account" "function_storage" {
-  name                     = "${var.app_prefix}stgacct"
+  name                     = "${var.storage_account_name}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
@@ -21,15 +21,22 @@ resource "azurerm_storage_table" "data_table" {
 }
 
 resource "azurerm_service_plan" "sp" {
-  name                = "${var.app_prefix}-service-plan"
+  name                = "${var.app_service_plan_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
   sku_name            = "Y1"
 }
 
-resource "azurerm_linux_function_app" "example" {
-  name                = "${var.app_prefix}-linux-function-app"
+resource "azurerm_application_insights" "app_insights" {
+  name                = var.app_insights_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "web"
+}
+
+resource "azurerm_linux_function_app" "function" {
+  name                = "${var.app_service_name}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
 
@@ -38,8 +45,22 @@ resource "azurerm_linux_function_app" "example" {
   service_plan_id            = azurerm_service_plan.sp.id
 
   app_settings = {
-    TEST_SETTING = "VALUE"
+    APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.app_insights.instrumentation_key
+    TABLE_STORAGE_CONN_STRING = azurerm_storage_account.function_storage.primary_connection_string
+    TABLE_STORAGE_TABLE_NAME = azurerm_storage_table.data_table.name
+    TARGET_TIME_ZONE = var.target_time_zone
+    WEATHER_API_ENDPOINT = var.weather_api_endpoint
+    WEATHER_API_KEY = var.weather_api_key
+    ZIP_CODE = var.zip_code
   }
 
   site_config {}
+
+  depends_on = [
+    azurerm_resource_group.rg,
+    azurerm_storage_account.function_storage,
+    azurerm_storage_table.data_table,
+    azurerm_service_plan.sp,
+    azurerm_application_insights.app_insights
+  ]
 }
--- IaC/variables.tf
+++ IaC/variables.tf
@@ -6,10 +6,42 @@ variable "resource_group_name" {
   type = string
 }
 
-variable "app_prefix" {
+variable "storage_account_name" {
   type = string
 }
 
 variable "storage_table_name" {
+  type = string
+}
+
+variable "app_service_plan_name" {
+  type = string
+}
+
+variable "app_service_name" {
+  type = string
+}
+
+variable "app_insights_name" {
+  type = string
+  default = "tf-weather-appinsights"
+}
+
+variable "target_time_zone" {
+  type = string
+  default = "Eastern Standard Time"
+}
+
+variable "weather_api_endpoint" {
+  type = string
+  default = "http://api.weatherapi.com/v1/"
+}
+
+variable "weather_api_key" {
+  type = string
+  sensitive = true
+}
+
+variable "zip_code" {
   type = string
 }
